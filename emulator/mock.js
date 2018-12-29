b23021583aebe11196d2db6a00dd45e0e450cf98
Łukasz Kieroński <lukasz.kieronski@gmail.com>
2018-12-29 16:55:40 +0100

    emulator

---
+++ emulator/mock.js
@@ -0,0 +1,58 @@
+const path = require('path')
+var args = require('minimist')(process.argv.slice(2), {
+    default: {
+        port: 1502
+    },
+    alias: {
+        f: 'file',
+        p: 'port',
+        h: 'help',
+    }
+});
+
+if (!args.f || args.h) {
+    console.log(`Usage: node ${path.basename(__filename)} -f FILE [-p PORT] [-h]`)
+    process.exit(1);
+}
+
+try {
+    const file = args.f[0] === '/' ? args.f : `./${args.f}`;
+    const data = require(file);
+
+    let modbus = require('jsmodbus')
+    let net = require('net')
+    let netServer = new net.Server()
+    let server = new modbus.server.TCP(netServer, {
+        holding: new Buffer(40000)
+    })
+
+    for (const address in data) {
+        const values = data[address];
+        for (let index = 0; index < values.length; index++) {
+            const value = values[index];
+            server.holding.writeUInt16BE(value, (+address+index)*2)
+        }
+    }
+
+    netServer.on('error', () => {
+        console.error(`Cannot run server on port ${args.p}.`);
+    })
+
+    netServer.on('listening', () => {
+        console.log(`Listening on port ${args.p}`)
+    })
+
+    netServer.listen(args.p)
+
+} catch (error) {
+    switch(error.code) {
+        case 'MODULE_NOT_FOUND':
+            console.error(`Cannot read file ${args.f}.`)
+            break
+        default:
+            console.error(`Unknown error ${error.code}.`)
+            break;
+    }
+}
+
+
